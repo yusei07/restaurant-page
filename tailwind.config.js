c2b2befabd2be7d30c421446aadeabe519576323
nora <usernamesaresoharddtomake@gmail.com>
2023-08-04 10:11:16 +0800

    add font & custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     './src/**/*.{html,js}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins'],
+      },
+      colors: {
+        orange: '#ed3c1f',
+        beige: '#ebdfce',
+        black: '#272624'
+      },
+    },
   },
   variants: {
     extend: {},
