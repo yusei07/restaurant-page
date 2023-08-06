a5186c24bf45e9e5d4c7b0cd4e9ffa2a99ea2a30
nora <usernamesaresoharddtomake@gmail.com>
2023-08-06 21:45:56 +0800

    responsive navbar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import './style.css';
 
-console.log("hello world");
+// feather icon
+feather.replace();
+
+function disableScroll() {
+  document.body.style.overflow = 'hidden';
+}
+
+function enableScroll() {
+  document.body.style.overflow = '';
+}
+
+// menu toggle
+const menuItems = document.querySelector("#menu-item");
+const menuIcon = document.querySelector("#open-btn");
+const closeIcon = document.querySelector("#close-btn");
+
+menuIcon.addEventListener('click', () => {
+  // hide menu icon
+  menuIcon.classList.add('hidden');
+  // display menu items
+  menuItems.classList.remove('hidden');
+  menuItems.classList.add('flex');
+  // display close icon
+  closeIcon.classList.remove('hidden');
+  disableScroll();
+})
+
+closeIcon.addEventListener('click', () => {
+  menuIcon.classList.remove('hidden');
+  menuItems.classList.add('hidden');
+  menuItems.classList.remove('flex');
+  closeIcon.classList.add('hidden');
+  enableScroll();
+})
