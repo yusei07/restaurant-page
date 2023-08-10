3f1b90910de559dc178fb6584ee2f22783d64cd7
nora <usernamesaresoharddtomake@gmail.com>
2023-08-10 11:09:33 +0800

    load landing page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,40 @@
 import './style.css';
+import { landingHTML, navHTML } from './html-components.js';
 
-// feather icon
-feather.replace();
+const container = document.querySelector("#container");
+
+window.addEventListener("load", () => {
+  container.innerHTML = "";
+  container.insertAdjacentHTML("beforeend", navHTML);
+  container.insertAdjacentHTML("beforeend", landingHTML);
+  displayNavItems();
+  feather.replace();
+})
+
+function displayNavItems() {
+  const menuItems = document.querySelector("#menu-item");
+  const menuIcon = document.querySelector("#open-btn");
+  const closeIcon = document.querySelector("#close-btn");
+
+  menuIcon.addEventListener('click', () => {
+    // hide menu icon
+    menuIcon.classList.add('hidden');
+    // display menu items
+    menuItems.classList.remove('hidden');
+    menuItems.classList.add('flex');
+    // display close icon
+    closeIcon.classList.remove('hidden');
+    disableScroll();
+  })
+
+  closeIcon.addEventListener('click', () => {
+    menuIcon.classList.remove('hidden');
+    menuItems.classList.add('hidden');
+    menuItems.classList.remove('flex');
+    closeIcon.classList.add('hidden');
+    enableScroll();
+  })
+}
 
 function disableScroll() {
   document.body.style.overflow = 'hidden';
@@ -11,26 +44,5 @@ function enableScroll() {
   document.body.style.overflow = '';
 }
 
-// menu toggle
-const menuItems = document.querySelector("#menu-item");
-const menuIcon = document.querySelector("#open-btn");
-const closeIcon = document.querySelector("#close-btn");
-
-menuIcon.addEventListener('click', () => {
-  // hide menu icon
-  menuIcon.classList.add('hidden');
-  // display menu items
-  menuItems.classList.remove('hidden');
-  menuItems.classList.add('flex');
-  // display close icon
-  closeIcon.classList.remove('hidden');
-  disableScroll();
-})
-
-closeIcon.addEventListener('click', () => {
-  menuIcon.classList.remove('hidden');
-  menuItems.classList.add('hidden');
-  menuItems.classList.remove('flex');
-  closeIcon.classList.add('hidden');
-  enableScroll();
-})
+// feather icon
+feather.replace();
