dc9b925f5022fa9ba06393cbcd468615c1cbc69d
nora <usernamesaresoharddtomake@gmail.com>
2023-08-14 16:44:44 +0800

    add nav component to story page

--- src/story.js
+++ src/story.js
@@ -1,7 +1,8 @@
-import { storyHTML } from './html-components';
+import { storyHTML, navHTML } from './html-components';
 
 const displayStory = (container) => {
   container.innerHTML = "";
+  container.insertAdjacentHTML("beforeend", navHTML);
   container.insertAdjacentHTML("beforeend", storyHTML);
   feather.replace();
 }
